4040c56d9079ade368e595be980ca6267988718d
David Noh <dnoh2019@gmail.com>
2020-12-08 11:37:12 -0800

    Completed Day One

--- QuestionOne/QuestionOne.java
+++ QuestionOne/QuestionOne.java
@@ -12,8 +12,37 @@ class QuestionOne {
         add(1456);
     }};
 
+    public void calculateWithTwoEntries(final ArrayList<Integer> listOfLines) {
+        for (Integer num : listOfLines) {
+            final int desiredValue = sum - num;
+            if (listOfLines.contains(desiredValue)) {
+                final int product = desiredValue * num;
+                System.out.println("Final Anwser for Two Entries is: " + product);
+                return;
+            }
+        }
+    }
+
+    public void calculateWithThreeEntries(final ArrayList<Integer> listOfLines) {
+        final int arrayLength = listOfLines.size();
+        for (int i = 0; i < arrayLength; i++) {
+            for (int j = 1; j < arrayLength; j++) {
+                final int firstValue = listOfLines.get(i);
+                final int secondValue = listOfLines.get(j);
+                final int desiredValue = sum - firstValue - secondValue;
+                if (listOfLines.contains(desiredValue)) {
+                    final int product = desiredValue * firstValue * secondValue;
+                    System.out.println("Final Anwser for Three Entries is: " + product);
+                    return;
+                }        
+            }
+        }
+    }
+
+
 
     public static void main(String[] args) {
+        QuestionOne questionOne = new QuestionOne();
         final ArrayList<Integer> listOfLines = new ArrayList<>();
         
         try (final BufferedReader bufReader = new BufferedReader(new FileReader("input.txt"))) {
@@ -26,14 +55,10 @@ class QuestionOne {
             System.out.println("Error reading in File. Exiting Program.");
             return;
         }
-
-        for (Integer num : listOfLines) {
-            final int desiredValue = sum - num;
-            if (listOfLines.contains(desiredValue)) {
-                System.out.println("Final Anwser is: " + desiredValue*num);
-                return;
-            }
-        }
+        
+        questionOne.calculateWithTwoEntries(listOfLines);
+        questionOne.calculateWithThreeEntries(listOfLines);
+        
     }
 
 }
